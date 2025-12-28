09b4ed76d9fb21358baf5b9b07ea896e31df4324
MOHAMED ALI ABDALLA MOHAMED <mohamed.ali.b2001@gmail.com>
2025-12-29 01:19:25 +0300

    Update src/main/shims.d.ts

---
+++ src/main/shims.d.ts
@@ -0,0 +1,8 @@
+declare module 'electron';
+declare module 'electron-log';
+declare module 'axios';
+declare module 'systeminformation';
+declare module 'node-machine-id';
+declare module 'uuid';
+declare module 'recharts';
+declare module 'lucide-react';
